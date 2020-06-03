506ec3c88d0f20a9bc35db3a8dd775c91a8780a3
Daniel Kelshaw <daniel.j.kelshaw@gmail.com>
2020-06-03 17:08:00 +0100

    Added ES Test

---
+++ tests/utils/test_early_stopping.py
@@ -0,0 +1,14 @@
+import torch
+from gpybo.utils.early_stopping import EarlyStopping
+
+
+class TestEarlyStopping:
+
+    def test_es(self):
+
+        es = EarlyStopping(patience=5, delta=0.01)
+        stop = False
+        for i in range(15):
+            stop = es(loss=torch.tensor(10.0))
+
+        assert stop
