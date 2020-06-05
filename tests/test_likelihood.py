cafdb7a98ecc8dd5fc3bafc582cd0ef5666d8b48
Daniel Kelshaw <daniel.j.kelshaw@gmail.com>
2020-06-05 17:28:27 +0100

    Updated Likelihood Test

--- tests/test_likelihood.py
+++ tests/test_likelihood.py
@@ -16,8 +16,9 @@ class TestGaussianLikelihood:
         kernel = SquaredExponentialKernel()
         x = torch.tensor([-4, -3, -2, -1], dtype=torch.float32)
         y = torch.sin(x)
+        noise = torch.tensor(0.0, dtype=torch.float32)
 
-        ll = gaussian_likelihood.log_likelihood(kernel, x, y)
+        ll = gaussian_likelihood.log_likelihood(kernel, x, y, noise)
         target = torch.tensor([-3.2324, -2.8482, -3.0372, -3.0269],
                               dtype=torch.float32)
 
